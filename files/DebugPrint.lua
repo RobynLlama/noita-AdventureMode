05fd815c37cfc5a1b8704f525f60d801422f3652
Mims <None>
2023-10-09 08:57:56 -0500

    added license info

--- files/DebugPrint.lua
+++ files/DebugPrint.lua
@@ -1,3 +1,15 @@
+--[[
+Module: dPrint
+Authors: MamaLlama, Flowerful
+Licensing:
+	This module and all of it component files are provied under the CC-BY-NC-4 license
+	A short and simple summary may be found at the following web address:
+	https://www.creativecommons.org/licenses/by-nc/4.0/deed.en
+
+	See the full legal code here:
+	https://www.creativecommons.org/licenses/by-nc/4.0/legalcode.en
+]]--
+
 --Quick little hack to enable globally controlling print statements
 --without needing to comment out each one
 local DEBUG_PRINT = false

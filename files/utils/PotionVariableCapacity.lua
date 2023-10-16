2e0f75dfb4720ceef2df09702131a399c6fa5492
Mims <None>
2023-10-15 22:10:42 -0500

    reorganized confusing file structure and made items inherit from base game items

--- files/utils/PotionVariableCapacity.lua
+++ files/utils/PotionVariableCapacity.lua
@@ -3,22 +3,51 @@
     overwrites the capacity to 1000 for some reason
 ]]
 
-local Entity = GetUpdatedEntityID()
-local components = EntityGetComponent(Entity, "VariableStorageComponent")
+dofile_once("mods/AdventureMode/files/utils/DebugPrint.lua")
 
-if (components ~= nil) then
-    for _, comp_id in pairs(components) do
-        local var_name = ComponentGetValue2(comp_id, "name")
-        if (var_name == "potion_material") then
-            potion_material = ComponentGetValue2(comp_id, "value_string")
-            potion_capacity = ComponentGetValue2(comp_id, "value_float")
+
+local OriginalInit = init
+
+function DetourInit(Entity)
+
+    --Run the original init first
+    OriginalInit(Entity)
+
+    local components = EntityGetComponent(Entity, "VariableStorageComponent")
+    local potion_material
+    local potion_capacity
+    local potion_clear
+
+    dPrint("Running on Entity: "..tostring(Entity), "PotionVariableCapacity", 1)
+    if (components ~= nil) then
+        dPrint("Found VariableStorageComponent", "PotionVariableCapacity", 1)
+        for _, comp_id in pairs(components) do
+            local var_name = ComponentGetValue2(comp_id, "name")
+            if (var_name == "potion_material") then
+                potion_material = ComponentGetValue2(comp_id, "value_string")
+                potion_capacity = ComponentGetValue2(comp_id, "value_float")
+                potion_clear = ComponentGetValue2(comp_id, "value_bool")
+            end
         end
     end
+
+    if (potion_clear) then
+        dPrint("Running potion clear", "PotionVariableCapacity", 1)
+        --Clear any other material and set it to the amount we specified
+        local Material = GetMaterialInventoryMainMaterial(Entity, false)
+        while (Material > 0) do
+            dPrint("Clearing "..CellFactory_GetName(Material), "PotionVariableCapacity", 1)
+            AddMaterialInventoryMaterial(Entity, CellFactory_GetName(Material), 0)
+            Material = GetMaterialInventoryMainMaterial(Entity, false)
+        end
+    end
+
+    if (potion_material == nil) or (potion_capacity == nil) then
+        return
+    end
+
+    dPrint("Adding new material", "PotionVariableCapacity", 1)
+    AddMaterialInventoryMaterial(Entity, potion_material, potion_capacity)
 end
 
-if (potion_material == nil) or (potion_capacity == nil) then
-    return
-end
-
---Set material amount
-AddMaterialInventoryMaterial(Entity, potion_material, potion_capacity)
+init = DetourInit
